aa4f747e6eb518dca8886196769d26420af0d4cf
James Burke <james.walter.burke@gmail.com>
2020-11-30 11:14:17 -0600

    Update demo with latest version of content fields

--- lib/class-fm-demo-content.php
+++ lib/class-fm-demo-content.php
@@ -9,7 +9,7 @@ if ( ! class_exists( 'FM_Demo_Content' ) ) :
 
 		private $plaintext = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-		private $markup = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. </p>
+		private $html = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. </p>
 <p>Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. <b>Vestibulum lacinia arcu eget nulla</b>. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. </p>';
 
 		private $markdown = '# Regna disposuit
@@ -96,15 +96,15 @@ debuit superis; ubi ramos non pollice.';
 				)
 			)->add_meta_box( 'Plaintext Example', 'demo-content' );
 
-			// Markup example.
+			// HTML example.
 			(
-				new Fieldmanager_Markup(
+				new Fieldmanager_HTML(
 					[
-						'name'         => 'markup_content',
-						'content'      => $this->markup,
+						'name'         => 'html_content',
+						'content'      => $this->html,
 					]
 				)
-			)->add_meta_box( 'Markup Example', 'demo-content' );
+			)->add_meta_box( 'HTML Example', 'demo-content' );
 
 			// Markdown example.
 			(
